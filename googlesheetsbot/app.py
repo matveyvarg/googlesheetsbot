9586020c435ce0c5e0c399c1915612b1918dbe03
Matvei <matveyvarg@gmail.com>
2025-10-06 20:55:11 +0200

    pass event loop

--- googlesheetsbot/app.py
+++ googlesheetsbot/app.py
@@ -11,6 +11,7 @@ from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from aiohttp import web
 
+from googlesheetsbot import google_client
 from googlesheetsbot.config import settings
 from googlesheetsbot.google_client import GoogleClient
 from googlesheetsbot.keyboards.cache import KeyboardCache
@@ -102,6 +103,9 @@ class App:
             dispatcher=dp,
             bot=self.bot,
             secret_token=settings.secret_token,
+            transaction=Transaction(),
+            google_client=google_client,
+            keyboard_cache=self.keyboard_cache,
         )
         # Register webhook handler on application
         webhook_requests_handler.register(app, path=settings.webhook_path)
@@ -110,7 +114,12 @@ class App:
         setup_application(app, dp, bot=self.bot)
 
         # And finally start webserver
-        web.run_app(app, host=settings.server.host, port=settings.server.port)
+        web.run_app(
+            app,
+            host=settings.server.host,
+            port=settings.server.port,
+            loop=asyncio.get_event_loop(),
+        )
 
 
 if __name__ == "__main__":
